f14e8bba82f588ca8e4d8408f7e76a902d00ea0a
mos0901386749 <99708987+mos0901386749@users.noreply.github.com>
2022-03-31 12:07:58 +0700

    first commit

---
+++ app/Views/namelist.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Codeigniter Crud</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<div class="container mt-4">
+<h1>ข้อมูล</h1>
+<hr>
+    <div class="d-flex justify-content-end">
+        <a href="<?php echo site_url('/addname') ?>" class="btn btn-primary">Add a Name & email</a>
+	</div>
+    <?php
+     if(isset($_SESSION['msg'])){
+        echo $_SESSION['msg'];
+      }
+     ?>
+  <div class="mt-3">
+     <table class="table table-bordered" id="users-list">
+       <thead>
+          <tr>
+             <th>User Id</th>
+             <th>ชื่อ-นามสกุล</th>
+             <th>Email</th>
+             <th>เวลาเข้า</th>
+             <th>เวลาออก</th>
+             <th>Action</th>
+          </tr>
+       </thead>
+       <tbody>
+          <?php if($users): ?>
+          <?php foreach($users as $user): ?>
+          <tr>
+             <td><?php echo $user['id']; ?></td>
+             <td><?php echo $user['name']; ?></td>
+             <td><?php echo $user['email']; ?></td>
+             <td><?php echo $user['time']; ?></td>
+             <td><?php echo $user['gettime']; ?></td>
+             <td>
+              <a href="<?php echo base_url('editnames/'.$user['id']);?>" class="btn btn-warning">แก้ไข</a>
+              <a href="<?php echo base_url('delete/'.$user['id']);?>" class="btn btn-danger">ลบ</a>
+              </td>
+          </tr>
+         <?php endforeach; ?>
+         <?php endif; ?>
+       </tbody>
+     </table>
+  </div>
+</div>
+ 
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+<script>
+    $(document).ready( function () {
+      $('#users-list').DataTable();
+  } );
+</script>
+</body>
+</html>
